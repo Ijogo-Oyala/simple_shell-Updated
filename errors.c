4b7b2bfff855178579bef67a8ad18bef921a12cf
Gift <ubekegift@gmai.com>
2023-08-21 04:23:41 -0700

    shells interactive mode

---
+++ errors.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+ *_eputs - prints the input string
+ * @str: String to be printed
+ *
+ * Return: returns Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - write(s) the character C to stderr
+ *
+ * @c: Character to print.
+ *
+ * Return: On success 1. On error, -1 is returned,
+ * 	and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - write(s) character C to given fd
+ * @c: Character(s) to print
+ *
+ * @fd: File descriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - print(s) an input string
+
+ * @str: String to be printed
+ *
+ * @fd: File descriptor to write to
+ *
+ * Return: Number of char(s) put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
