8e27ce0e3a7ca7e0873f2076450a23f1fd082ce6
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-08-23 18:16:29 -0700

    Contains utility functions for tokenization, execution, memory reallocation, and freeing memory. - Shali

---
+++ tok_execute_realloc_free.c
@@ -0,0 +1,108 @@
+#include "simple_shell.h"
+
+/**
+ * _realloc_ - Reallocates memory block with a new size.
+ * @prev_ptr: Pointer to the previous memory block.
+ * @old_size: Previous size of the memory block.
+ * @new_size: New size for the memory block.
+ * Return: Pointer to the newly resized memory block.
+ */
+void *_realloc_(void *prev_ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *old_ptr;
+	char *new_ptr;
+
+	unsigned int j;
+	/* If previous pointer is NULL, allocate new memory */
+	if (prev_ptr == NULL)
+		return (malloc(new_size));
+	/* If new size is the same as old size, return previous pointer */
+	if (new_size == old_size)
+		return (prev_ptr);
+	/* If new size is 0 and previous pointer is not NULL, free memory and return NULL */
+	if (new_size == 0 && prev_ptr != NULL)
+	{
+		free(prev_ptr);
+		return (NULL);
+	}
+	/* Allocate memory for the new size */
+	new_ptr = malloc(new_size);
+	old_ptr = prev_ptr;
+
+	if (new_ptr == NULL)
+		return (NULL);
+	/* Copy data from old pointer to new pointer */
+	if (new_size > old_size)
+	{
+		for (j = 0; j < old_size; j++)
+			new_ptr[j] = old_ptr[j];
+		free(prev_ptr);
+		for (j = old_size; j < new_size; j++)
+			new_ptr[j] = '\0';
+	}
+	if (new_size < old_size)
+	{
+		for (j = 0; j < new_size; j++)
+			new_ptr[j] = old_ptr[j];
+		free(prev_ptr);
+	}
+	return (new_ptr);
+}
+
+
+/**
+ * split_string - Splits a string and creates an array of pointers to words.
+ * @str: The string to be split.
+ * @delim: The delimiter to split the string.
+ * Return: An array of pointers to words.
+ */
+char **split_string(char *str, const char *delim)
+{
+	int index, word_count;
+	char **word_array;
+	char *tokens, *str_copy;
+
+	/* Create a copy of the input string to avoid modifying the original */
+	str_copy = malloc(str_len(str) + 1);
+	if (str_copy == NULL)
+	{
+		perror(_get_env("_"));
+		return (NULL);
+	}
+	index = 0;
+	while (str[index])
+	{
+		str_copy[index] = str[index];
+		index++;
+	}
+	str_copy[index] = '\0';
+
+	/* Get the first token */
+	tokens = strtok(str_copy, delim);
+
+	/* Initialize the word array with the first token */
+	word_array = malloc(sizeof(char *) * 2);
+	word_array[0] = str_dup(tokens);
+
+	index = 1;
+	word_count = 3;
+
+	/* Loop through the remaining tokens and add them to the word array */
+	while (tokens)
+	{
+		tokens = strtok(NULL, delim);
+
+		/* Resize the word array and add the token */
+		word_array = _realloc_(word_array, (sizeof(char *) * (word_count - 1)), (sizeof(char *) * word_count));
+		word_array[index] = str_dup(tokens);
+
+		index++;
+		word_count++;
+	}
+
+	/* Free the copied string */
+	free(str_copy);
+
+	return (word_array);
+}
+
