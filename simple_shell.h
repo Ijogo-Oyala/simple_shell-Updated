c265fc341bacf8ff6b444c5900111c924ce7ab6e
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-08-23 18:12:32 -0700

    Header file defining structures, function prototypes, and includes. - Shali

--- simple_shell.h
+++ simple_shell.h
@@ -48,3 +48,34 @@ path_dir *add_path_node_end(path_dir **head, char *dir);
 path_dir *link_path_dir(char *path_string);
 char *_which_(char *filename, path_dir *path_list);
 
+/**
+ * struct built_in - Holds a pointer to a function
+ * along with its corresponding built-in command.
+ *
+ * @com_name: The built-in command's name.
+ * @function: A function pointer to execute the corresponding built-in command.
+ */
+typedef struct built_in
+{
+	char *com_name;
+	void (*function)(char **);
+} built_in;
+
+/*--- builtin functions ---*/
+int _atoi_(char *str);
+void _env_(char **args __attribute__((unused)));
+void _set_env(char **args);
+void _unset_env(char **args);
+void _exit_(char **args);
+
+/*--- builtin finder function ---*/
+void(*_find_builtin(char **com))(char **);
+
+/*--- memory leak handling ---*/
+void free_arv(char **arv);
+
+void free_path_list(path_dir *head); /*pathfinder.c */
+
+
+#endif /* SIMPLE_SHELL_H */
+
